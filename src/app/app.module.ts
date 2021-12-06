8ea2c04b2c233bd57038b4ed85e248d1c34bdab2
AlexAkiu <alexanderakiu@gmail.com>
2021-12-06 08:52:12 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TodoItemsComponent } from './todo-items/todo-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoItemsComponent
   ],
   imports: [
     BrowserModule
